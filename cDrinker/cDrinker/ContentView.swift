bbf58fe3bc5547f65d05a03a9dce2fa2a181e8d3
anubhavsingh16 <anubhavssingh177@gmail.com>
2021-02-04 11:35:46 +0530

    Cleaning up the UI

--- cDrinker/cDrinker/ContentView.swift
+++ cDrinker/cDrinker/ContentView.swift
@@ -27,82 +27,74 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
-//    static var defaultWakeTime: Date {
-//
-//        var defaultWakeTime: Date {
-//               var components = DateComponents()
-//               components.hour = 7
-//               components.minute = 0
-//               return Calendar.current.date(from: components) ?? Date()
-//        }
-//    }
-    
     var body: some View {
-
-    NavigationView {
-        VStack{
-            Texttitle(title: "When you want to wakeup?")
-           
-            DatePicker("Please enter time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-            .labelsHidden()
-
-            Texttitle(title: "How much you want to Sleep?")
-            
-            Stepper(value: $sleepAmount, in: 4...12.0, step: 0.25){
-                
-                Text("\(sleepAmount, specifier: "%g") Hours")
-            }.padding()
-            
-            Texttitle(title: "How much Coffee you want to drink")
-            
-            Stepper(value: $coffeeAmount, in: 1...12, step: 1){
-                if coffeeAmount == 1{
-                    Text("\(coffeeAmount, specifier: "%g") cup")
-                }else{
-                   Text("\(coffeeAmount, specifier: "%g") cups")
+        
+        NavigationView {
+            Form{
+                VStack(alignment: .leading, spacing: 0){
+                    Texttitle(title: "When you want to wakeup?")
+                    
+                    DatePicker("Please enter time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
+                    
+                    Texttitle(title: "How much you want to Sleep?")
+                    
+                    Stepper(value: $sleepAmount, in: 4...12.0, step: 0.25){
+                        
+                        Text("\(sleepAmount, specifier: "%g") Hours")
+                    }.padding()
+                    
+                    Texttitle(title: "How much Coffee you want to drink")
+                    
+                    Stepper(value: $coffeeAmount, in: 1...12, step: 1){
+                        if coffeeAmount == 1{
+                            Text("\(coffeeAmount, specifier: "%g") cup")
+                        }else{
+                            Text("\(coffeeAmount, specifier: "%g") cups")
+                        }
+                        
+                    }.padding()
+                    .alert(isPresented: $showingAlert) {
+                        Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                    }
                 }
-                
-            }.padding()
-                
-            .alert(isPresented: $showingAlert) {
-                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
             }
-      }
-        .navigationBarTitle("cDrinker", displayMode: .inline)
-        .navigationBarItems(trailing:
-            Button(action: calculateBedTime) {
-                Text("Calculate")
-            }
-        )
-
+            .navigationBarTitle("cDrinker", displayMode: .inline)
+            .navigationBarItems(trailing:
+                Button(action: calculateBedTime) {
+                    Text("Calculate")
+                })
         }
-  }
+    }
     func calculateBedTime() {
-
-        //connect model with swift
+        
+        //connect ML model with swift
         let model = SleepCalculator()
         
-        // change the value of time into seconds
+        // wakeup time
+        // We can then request the hour and minute components, and pass in our wake up date.
         let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
         let hour = (components.hour ?? 0) * 60 * 60
         let minute = (components.minute ?? 0) * 60
         
         self.showingAlert = true
-
+        
         do {
-
-            // gives time in second for ideal sleep
-           let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-
+            // feed our values into Core ML and see what comes out. This wants the wake time, estimated sleep, and coffee amount values required to make a prediction.
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            //  prediction now contains how much sleep they actually need.
+            
             let sleepTime = wakeUp - prediction.actualSleep
+            
             let formatter = DateFormatter()
             formatter.timeStyle = .short
             
-            
             alertTitle = "Your ideal bedtime is:"
-            
             alertMessage = formatter.string(from: sleepTime)
-
+            
         } catch {
             alertTitle = "Error"
             alertMessage = "Sorry there was problem in calculating your bedtime"
